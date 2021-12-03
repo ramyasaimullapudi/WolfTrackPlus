c00d1ad4e686d3b679292057524d0e8f833a87ce
arjunptm <arjunptm@gmail.com>
2021-12-03 14:27:06 -0500

    Fixes #43 failing test cases

--- Controller/home.py
+++ Controller/home.py
@@ -31,12 +31,13 @@ def login():
 
 @home_route.route("/auth", methods=["GET"])
 def auth():
-    if 'email' in session: 
-        data = user.get_auth_user_dao(session['email'])
-        data["wishlist"] = application.get(session['email'], '')
-        return render_template('home.html', data=data, upcoming_events=upcoming_events)
+    if "email" in session:
+        data = user.get_auth_user_dao(session["email"])
+        data["wishlist"] = application.get(session["email"], "")
+        return render_template("home.html", data=data, upcoming_events=upcoming_events)
     else:
         return redirect("/login")
+
 
 @home_route.route("/loginUser", methods=["GET", "POST"])
 def loginUser():
@@ -221,8 +222,8 @@ def edit_profile():
 @home_route.route("/logout", methods=["GET"])
 # @login_required
 def logout():
-    if 'email' in session:  
-        session.pop('email', None) 
+    if "email" in session:
+        session.pop("email", None)
     # logout_user()
     return redirect("/login")
 
