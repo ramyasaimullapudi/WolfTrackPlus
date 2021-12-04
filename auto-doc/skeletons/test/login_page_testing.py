32c8914276d496dc4bb8a045d253907a242d7c1b
rmullap <rmullap@ncsu.edu>
2021-12-04 14:16:14 -0500

    added doc strings for user_applications_testing and body for login_testing

---
+++ auto-doc/skeletons/test/login_page_testing.py
@@ -0,0 +1,53 @@
+class FlaskTest():
+    """
+    It is the subject body to write all unit testcases for login page
+    """
+
+def test_index(self):
+    """
+
+    """
+def test_index_content(self):
+    """
+
+    """
+
+def test_valid_email(self):
+    """
+
+    """
+
+def test_validate_credentials(self):
+    """
+
+    """
+
+def test_validate_credentials(self):
+    """
+
+    """
+
+def test_repeated_signup(self):
+    """
+
+    """
+
+def test_new_application(self):
+    """
+
+    """
+
+def test_edit_application(self):
+    """
+
+    """
+
+def test_edit_profile(self):
+    """
+
+    """
+
+def test_edit_profile(self):
+    """
+
+    """
